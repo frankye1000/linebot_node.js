24a6fe0b608acca50130ab5bdddef53cb6513ecb
frankye1000 <33119231+frankye1000@users.noreply.github.com>
2018-05-08 21:09:06 +0800

    make it better

--- index.js
+++ index.js
@@ -24,21 +24,21 @@ bot.on('message', function (event) {
   altText: 'this is a buttons template',
   template: {
     type: 'buttons',
-    thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
-    title: 'Menu',
-    text: 'Please select',
+    thumbnailImageUrl: 'http://chennie1982.pixnet.net/blog/post/133478680-3-1-%E5%A4%A7%E6%A9%8B%E5%9C%8B%E5%B0%8F%E6%84%9B%E5%BF%83%E5%A0%B4',
+    title: '老婆福利',
+    text: '請選擇',
     actions: [{
-      type: 'postback',
-      label: 'Buy',
-      data: 'action=buy&itemid=123'
+      type: 'message',
+      label: '老婆稱讚你',
+      text: '噁心!死肥宅!誰是你老婆! '
     }, {
       type: 'postback',
       label: 'Add to cart',
       data: 'action=add&itemid=123'
     }, {
       type: 'uri',
-      label: 'View detail',
-      uri: 'http://example.com/page/123'
+      label: '老婆性感照',
+      uri: 'https://tw.appledaily.com/new/realtime/20150707/642761/'
     }]
   }
 });
